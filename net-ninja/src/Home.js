f18c9e2fb3e2c45c4a8c9b951f2c6fa75a46de2a
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-18 17:58:44 +0200

    Handles Remove

--- net-ninja/src/Home.js
+++ net-ninja/src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: "My new website", body: "Lorem ipsum... ", author: "mario", id: 1 },
+        {title: "Welcome Party!", body: "Lorem ipsum... ", author: "yoshi", id: 2 },
+        {title: "My dev top tips", body: "Lorem ipsum... ", author: "mario", id: 3 }
+    ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    };
+
     return (
         <div className="home">
-            <h2>HomePage</h2>
+            <BlogList blogs={ blogs } title={ "All Blogs!" } handleDelete = {handleDelete}/>
         </div>
     );
 }
